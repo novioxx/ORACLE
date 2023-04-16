d59cca673094b095bb4a88ce898f5a8250cffb82
novioxx <novioxx@naver.com>
2023-04-17 00:46:55 +0900

    oracle

--- homework/2023.04.11/02_SELECT_Question.sql
+++ homework/2023.04.11/02_SELECT_Question.sql
@@ -1,16 +1,17 @@
 --1) 각 학생의 평점을 검색하라(별칭을 사용)
-
+SELECT ST.SNAME, ST.AVR FROM STUDENT ST;
 
 --2) 각 과목의 학점수를 검색하라(별칭을 사용)
-
+SELECT C.CNAME, C.ST_NUM FROM COURSE C;
 
 --3) 각 교수의 지위를 검색하라(별칭을 사용)
-
+SELECT P.ORDERS, P.PNAME FROM PROFESSOR P; 
 
 --4) 급여를 10%인상했을 때 연간 지급되는 급여를 검색하라(별칭을 사용)
-
+SELECT E.SAL * 1.1 FROM EMP E;
 
 --5) 현재 학생의 평균 평점은 4.0만점이다. 이를 4.5만점으로 환산해서 검색하라(별칭을 사용)
+SELECT ST.SNO, ST.SNAME, ST.AVR * 4.5 / 4.0 FROM STUDENT ST;
 
 
 
--- homework/2023.04.11/05_SELECT_Question.sql
+++ homework/2023.04.11/05_SELECT_Question.sql
@@ -1,27 +1,28 @@
 --1) 화학과 학생을 검색하라
-
+SELECT MAJOR, SNAME FROM STUDENT WHERE MAJOR = '화학';
 
 --2) 평점이 2.0 미만인 학생을 검색하라
-
+SELECT AVR, SNAME, SNO FROM STUDENT WHERE AVR >= 2.0;
 
 --3) 관우 학생의 평점을 검색하라
-
+SELECT SNO,SNAME, AVR FROM STUDENT WHERE SNAME LIKE '관%';
 
 --4) 정교수 명단을 검색하라
+SELECT ORDERS, PNAME FROM PROFESSOR WHERE ORDERS = '정교수';
 
 
 --5) 화학과 소속 교수의 명단을 검색하라
-
+SELECT SECTION, PNAME FROM PROFESSOR WHERE SECTION = '화학';
 
 --6) 송강 교수의 정보를 검색하라
-
+SELECT * FROM PROFESSOR WHERE PNAME = '송강';
 
 --7) 학년별로 화학과 학생의 성적을 검색하라
-
+SELECT SYEAR, SNAME, AVR FROM STUDENT ORDER BY SYEAR;
 
 --8) 2000년 이전에 부임한 교수의 정보를 부임일순으로 검색하라
-
+SELECT PNAME,HIREDATE FROM PROFESSOR WHERE HIREDATE < TO_DATE('20000101', 'YYYYMMDD');
 
 --9) 담당 교수가 없는 과목의 정보를 검색하라
-
+SELECT * FROM COURSE WHERE PNO IS NULL;
 
